c0532180f7b0e7938e953619fb813773ce8fd05a
Marco Vergaray <mvergaray@belatrixsf.com>
2016-07-19 21:09:44 -0500

    Initial Commit

---
+++ PokeWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeWatch
+//
+//  Created by Marco Vergaray Guerra on 7/19/16.
+//  Copyright © 2016 Marco Vergaray Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
